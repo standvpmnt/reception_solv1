a389a9d9f3bde684af8822be6adcaf4422c3440e
Standvpmnt <shubhang.saltech@gmail.com>
2019-09-29 05:03:17 +0530

    Searching test requests in short_name as well

--- app/models/test_rate.rb
+++ app/models/test_rate.rb
@@ -2,7 +2,7 @@ class TestRate < ApplicationRecord
 
 	def self.lookup(test_name)
 		if !(test_name.blank?)
-			@tests = TestRate.where('test_name LIKE ?', "%#{test_name}%")
+			@tests = TestRate.where('test_name LIKE ?', "%#{test_name}%").or(TestRate.where('test_short_name LIKE ?', "%#{test_name}%"))
 		end
 	end
 
